f19c373834425c860e6e01e12de7c73fd3a23962
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-05-17 22:09:03 +0800

    不打包vuex

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -39,6 +39,7 @@ export default {
     libraryTarget: 'jsonp',
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  externals: { vuex: 'vuex' },
   module: {
     rules: [
       { test: /\.vue/, exclude: /node_modules/, use: [vueLoader] },
